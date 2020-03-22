4c403e96fd6c92db22313201758e32b53aa7ce22
MariaSyrokvash <mariya.syrokvash@gmail.com>
2020-03-22 19:13:20 +0300

    feat: add task-solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
 module.exports = function toReadable (number) {
-  
+const NUMBERS = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+    100: 'hundred',
+    }; 
+
+    let result = '';
+
+    let oneDigitNumber = number % 10;
+    let decades = Math.floor(number % 100 / 10);
+    let hundreds = Math.floor(number / 100);
+
+
+    if (number === 0) {
+    return 'zero'
+    };
+
+
+    if (hundreds > 0) {
+    result = result + NUMBERS[hundreds];
+    result = result + ' hundred';
+    };
+
+    if (decades > 1) {
+    if (hundreds > 0) {
+        result = result +  ' ';
+    }
+    result = result +  NUMBERS[decades * 10];
+
+    if (oneDigitNumber > 0) {
+        result = result + ' ';
+        result = result + NUMBERS[oneDigitNumber];
+        }
+    }
+
+    else if (decades === 1 || oneDigitNumber > 0) {
+    if (hundreds > 0) {
+        result = result + ' ';
+    }
+    result = result + NUMBERS[decades * 10 + oneDigitNumber];
+    }
+
+
+    return result;
 }
